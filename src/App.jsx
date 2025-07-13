265a72d7912da5cfaa2394cc2619d33ae25e61db
Justin Padilla <justinpadilla117@gmail.com>
2025-07-13 16:03:09 +0800

    Adding RGB Inputbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import style from "./App.module.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { container, baseBox, rgbContainer, rgbContent, rgbForm } = style;
+  const rgbLabel = ["Red", "Green", "Blue"];
 
+  const labelColorMap = {
+    Red: "red",
+    Green: "green",
+    Blue: "blue"
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className={container}>
+        <div className={baseBox}></div>
 
-export default App
+        <div className={rgbContainer}>
+          {rgbLabel.map((label, index) => {
+            return (
+              <div className={rgbContent} key={index}>
+                <label style={{ color: labelColorMap[label] }}>{label}</label>
+                <input className={rgbForm} type="number" name={label} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
